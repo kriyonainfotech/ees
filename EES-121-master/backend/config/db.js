a7c8e1a2a50003e8c6f583dc1f702df1f6aa7019
sujal <sujaltank12@gmail.com>
2024-12-05 18:01:34 +0530

    savee

--- EES-121-master/backend/config/db.js
+++ EES-121-master/backend/config/db.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose')
-require('dotenv').config();
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect("mongodb+srv://rajivmsurati11:rajivmsurati11@cluster0.p9hso.mongodb.net/EES-121_App")
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
-    } catch (error) {
-        console.log(error);
-        return false;
-
+    const uri = "mongodb+srv://rajivmsurati11:rajivmsurati11@cluster0.p9hso.mongodb.net/EES-121_App"; // Ensure this variable exists in your .env file
+    if (!uri) {
+        throw new Error("MongoDB URI is not defined in environment variables");
     }
-}
+    try {
+        await mongoose.connect(uri);
+        console.log("MongoDB connected successfully");
+    } catch (err) {
+        console.error("Error connecting to MongoDB", err.message);
+        process.exit(1);
+    }
+};
+
 module.exports = connectDB;
--- EES-121-master/backend/ecosystem.config.js
+++ EES-121-master/backend/ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
             script: "./index.js",
             env: {
                 NODE_ENV: "production",
-                PORT: 3000,
+                PORT: 3001,
                 MONGO_URI: "mongodb+srv://rajivmsurati11:rajivmsurati11@cluster0.p9hso.mongodb.net/EES-121_App",
             },
         },
